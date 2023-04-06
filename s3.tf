016eb58d4d29d0ce361f6709ae5133edd34726a1
Omshree Butani <71128303+Omshree999@users.noreply.github.com>
2023-04-06 22:07:01 +0530

    Add files via upload

---
+++ s3.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "bomb" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.bomb.id
+  acl    = "private"
+}
